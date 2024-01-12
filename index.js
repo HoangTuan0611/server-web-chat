4dcad0b06fa2286b8eb189b4b0ac933a2b6fe14d
chuotnhat <tranhuong13520340@gmail.com>
2024-01-12 16:27:53 +0700

    edit config

--- index.js
+++ index.js
@@ -8,15 +8,10 @@ var cors = require('cors');
 app.use(cors());
 
 const socketIo = require( "socket.io" )( server, {
-  cors: { origin : '*',},
-  handlePreflightRequest: (req, res) => {
-    res.writeHead(200, {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,POST",
-      "Access-Control-Allow-Headers": "my-custom-header",
-      "Access-Control-Allow-Credentials": true
-    });
-    res.end();
+  cors: {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
   }
 });
 
