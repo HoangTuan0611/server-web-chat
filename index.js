f4ffb81ed69ad8d93817224f206f9b90b3fcce4b
chuotnhat <tranhuong13520340@gmail.com>
2024-01-12 15:00:49 +0700

    update config

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const http = require( "http" );
 var app = express();
 const server = http.createServer( app );
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://client-web-chat.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
 const socketIo = require( "socket.io" )( server, {
   cors: {
     origin: "*",
